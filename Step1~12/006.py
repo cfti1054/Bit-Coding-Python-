b2d78b1369e94a65f07c4e17deb6647f5b303872
SuHyeok <cfti1054@naver.com>
2022-04-01 15:50:27 +0900

    First Commit

---
+++ Step1~12/006.py
@@ -0,0 +1,8 @@
+print(14590) # 14590
+#print(abc) # error
+print(13-2) # 11
+print(12 + 4 / 2 + 1) # 15.0
+print("A" + "B" + "C") # ABC
+print("A" - "B") # error
+print("A" + "B") # AB
+print(52 - "33") # error
---
+++ Step1~12/080.py
@@ -0,0 +1,5 @@
+string = "abcdefghijklmn"
+
+num1 = 4
+num2 = 8
+print(string[num1:num2])
